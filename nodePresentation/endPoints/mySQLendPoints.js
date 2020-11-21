742f8522f7710f1900370df98d027aca1013435b
Your Name <you@example.com>
2020-11-21 16:01:36 +0000

    added table content

--- nodePresentation/endPoints/mySQLendPoints.js
+++ nodePresentation/endPoints/mySQLendPoints.js
@@ -16,5 +16,26 @@ function connectionTest(req,res){
 	res.send({"status":"test"})
 }
 
+
+function getAlldata(req,res){
+	var con = mysql.createConnection({
+		host: "localhost",
+		user: "tzannetos",
+		password: "#tzannetos1",
+		database:"softeng2020demo"
+	});
+	con.connect(function(err) {
+		if (err) throw err;
+		console.log("Connected!");
+		con.query("SELECT * FROM evchargingpoints", function (err, result, fields){
+			if (err) throw err;
+			res.send(result);
+			  });
+	});
+	//res.send("connection suceeded");
+
+}
+
 router.get('/' + 'testmySqlConnection',connectionTest);
+router.get('/'+'mySqlTable',getAlldata);
 module.exports = router; 
