5a6cecd9fd6ea0792e2c43188ffd6bc8b82c7765
Ioannis Tzannetos <tzannetosg@gmail.com>
2020-11-22 21:15:02 +0200

    changed structure

--- nodePresentation/index.js
+++ nodePresentation/index.js
@@ -1,26 +1,31 @@
 const express = require('express')
 const app = express();
 const port = 8000;
-var bodyParser = require('body-parser')
-app.use( bodyParser.json() );  // add middleware to parse body of requests as JSON
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
-})); 
+
+
+
 //initialize port for node application to run
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`)
+  console.log(`Example app listening on port ${port}!`);
 });
 
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!');
 });
 
 
-const sqlite3router=require("./endPoints/sqlite3endPoints");
+// load all endpoints
+const allData=require("./endpoints/allData.js");
+const allMunicipalities=require("./endpoints/allMunicipalities.js");
+const dataByMunicipality=require("./endpoints/dataByMunicipality");
+const allNames=require("./endpoints/allNames");
+const dataByName=require("./endpoints/dataByName");
 
-const mySqlrouter=require("./endPoints/mySQLendPoints.js");
-
-app.use('/', sqlite3router);
-app.use("/",mySqlrouter);
+//bind all endpoints to app router
+app.use('/',allData);
+app.use('/',allMunicipalities);
+app.use('/',dataByMunicipality);
+app.use('/',allNames);
+app.use('/',dataByName);
